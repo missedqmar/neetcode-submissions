52f119f56e226baef80ae2cec783a3ec693b395c
Marvin AC <MarvinAstonitas@gmail.com>
2026-09-18 16:01:13 -0400

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        if(nums == null) return false;
+        if(nums.length == 1 || nums.length == 0) return false;
+
+        Map<Integer,Boolean> seen = new HashMap<>();
+
+        for(int curr: nums){
+                if(seen.containsKey(curr)){
+                    return true;
+                }else{
+                    seen.put(curr,true);
+                }
+        }
+
+        return false;
+    }
+}
